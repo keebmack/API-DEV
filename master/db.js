bc31ccb82406d5c5fad5180a1402aac968529867
keebmack <rmkebe@pointpark.edu>
2023-08-20 18:21:25 -0400

    setting up cloud environment

--- master/db.js
+++ master/db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "ryank",
-    password: "ryankebe",
+    user: "ryankebe",
+    host: "dpg-cjh8qhr6fquc73dsl3sg-a",
+    database: "kebe",
+    password: "7fyy90iGOgEgCrDYUdjauHipMrYAMwoH",
     port: 5432,
 });
 
